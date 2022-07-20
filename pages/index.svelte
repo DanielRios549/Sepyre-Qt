6319385b608908c81c7c311666d14cb7842e5593
Daniel Rios <danielrios.flamengo@gmail.com>
2022-07-20 13:19:52 -0300

    Move Grid

--- pages/index.svelte
+++ pages/index.svelte
@@ -13,6 +13,18 @@
 <Footer/>
 
 <style lang="postcss">
+    :global(body) {
+        $header: 60px;
+        $footer: 20px;
+        overflow-y: hidden;
+        display: grid;
+        grid-template:
+            "header header" $header
+            "aside main" calc(100vh - ($header + $footer))
+            "footer footer" $footer
+            / min-content
+        ;
+    }
     main {
         grid-area: main;
         height: 100vh;

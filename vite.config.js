2078ca8d77fcc251573cac9787eb88666a058915
Daniel Rios <danielrios.flamengo@gmail.com>
2022-07-22 19:39:48 -0300

    Update Vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { resolve } from 'path'
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit(), svg()],
+    server: {
+        port: 3000
+    },
     build: {
         polyfillModulePreload: false
     },
